86c22bf9acef08d88ed5062df11b9ab89ee772c3
oberon10 <prosperbernrad@gmail.com>
2023-03-22 05:37:20 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h'
+/**
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (success)
+ */
+ void print_alphabet(void)
+ {
+	 int i;
+
+	 for (i = 97; i<= 122; i++)
+	{
+		_putchar(i);
+	}
+	 _putchar('/n');
+}
