db0f542442be8bc10d4a874aaa128fd38cbd5e65
oberon10 <prosperbernrad@gmail.com>
2023-03-22 05:30:35 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,18 @@
-#include "main.h" 
-/** 
-* main - print _putchar  
-* Return: 0 
-*/ 
-int main(void) 
-{ 
-_putchar('_'); 
-_putchar('p'); 
-_putchar('u'); 
-_putchar('t'); 
-_putchar('c'); 
-_putchar('h'); 
-_putchar('a'); 
-_putchar('r'); 
-_putchar('\n'); 
-return (0); 
+#include "main.h"
+/**
+ * * main - Entry point
+ * Description: 'prints _putchar to stdout'
+ * Return: always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	Return (0);
 }
