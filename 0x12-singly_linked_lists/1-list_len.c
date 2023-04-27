d324886033c20339c88687cd5b41465e4dc38b5f
oberon10 <prosperbernrad@gmail.com>
2023-04-27 11:48:29 -0700

    update commit

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,13 +9,13 @@
  */
 size_t list_len(const list_t *h)
 {
- size_t n = 0;
+	size_t n = 0;
 
- while (h)
- {
- n++;
- h = h->next;
- }
- return (n);
+	while (h)
+	{
+	n++;
+	h = h->next;
+	}
+	return (n);
 }
 
